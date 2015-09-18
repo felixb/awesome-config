075c39c58481001d41a9049e01914bfa3f9922cc
Felix Bechstein <felix.bechstein@otto.de>
2015-09-18 07:41:23 +0200

    support setting font size for xfce terminal

--- bin/setfontsize.sh
+++ bin/setfontsize.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-printf '\33]50;%s\007' "xft:terminus-font:size=${1:-10}"
+if [ "${TERM}" = 'xrvt' ] ; then
+  printf '\33]50;%s\007' "xft:terminus-font:size=${1:-10}"
+else
+  sed -i ~/.config/xfce4/terminal/terminalrc -e "/FontName=/s/[0-9]*\$/${1:-9}/"
+fi
