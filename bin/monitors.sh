ad2a16fc7dc2b8d2dac87150381a43a239a046f3
Felix Bechstein <f@ub0r.de>
2015-09-26 15:17:18 +0200

    add TV/VGA output to monitors.sh

--- bin/monitors.sh
+++ bin/monitors.sh
@@ -1,18 +1,33 @@
 #!/bin/sh
 
 xrandr -q &> /dev/null
-if $(xrandr --prop |grep -q "DP2-2 connected");then
-		xrandr --output DP2-2 --off --output DP2-3 --off
-		xrandr \
-                       --output eDP1 --mode 1366x768 --pos 0x0 --rotate normal \
-                       --output DP2-2 --mode 1680x1050 --pos 1680x0 --rotate normal \
-                       --output DP2-3 --mode 1680x1050 --pos 3360x0 \
-                       --output HDMI2 --off \
-                       --output VIRTUAL1 --off \
-                       --output DP1 --off \
-                       --output DP2-1 --off \
-                       --output HDMI1 --off \
-                       --output DP2 --off
+
+if [ "${1}" = 'tv' ] ; then
+  xrandr \
+    --output eDP1 --mode 1366x768 --pos 0x0 \
+    --output VGA1 --mode 1360x768 --right-of LVDS1
+elif $(xrandr --prop | grep -q "DP2-2 connected"); then
+  xrandr \
+    --output eDP1 --mode 1366x768 --pos 0x0 \
+    --output DP2-2 --mode 1680x1050 --pos 1680x0 \
+    --output DP2-3 --mode 1680x1050 --pos 3360x0 \
+    --output HDMI2 --off \
+    --output VIRTUAL1 --off \
+    --output DP1 --off \
+    --output DP2 --off \
+    --output DP2-1 --off \
+    --output HDMI1 --off \
+    --output VGA1 --off
 else #if it's not connected
-    xrandr --output eDP1 --mode 1366x768 --pos 0x0
+  xrandr \
+    --output eDP1 --mode 1366x768 --pos 0x0 \
+    --output DP2-2 --off \
+    --output DP2-3 --off \
+    --output HDMI2 --off \
+    --output VIRTUAL1 --off \
+    --output DP1 --off \
+    --output DP2 --off \
+    --output DP2-1 --off \
+    --output HDMI1 --off \
+    --output VGA1 --off
 fi
