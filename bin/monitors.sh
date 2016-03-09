3ba1f81bc4b38567a77b65a34ad4d8121ddd308e
Felix Bechstein <felix.bechstein@otto.de>
2016-03-09 08:18:47 +0100

    add pairing mode to monitors.sh

--- bin/monitors.sh
+++ bin/monitors.sh
@@ -17,7 +17,12 @@ if [ "${1}" = "off" ] ; then
 elif [ "${1}" = 'tv' ] ; then
   xrandr \
     --output eDP1 --mode 1366x768 --pos 0x0 \
-    --output VGA1 --mode 1360x768 --right-of LVDS1
+    --output VGA1 --mode 1360x768 --right-of eDP1
+elif [ "${1}" = 'pair' ] ; then
+  xrandr \
+    --output eDP1 --mode 1366x768 --pos 0x0 \
+    --output DP2-2 --mode 1680x1050 --pos 1680x0 \
+    --output DP2-3 --mode 1680x1050 --same-as DP2-2
 elif $(xrandr --prop | grep -q "DP2-2 connected"); then
   xrandr \
     --output eDP1 --mode 1366x768 --pos 0x0 \
