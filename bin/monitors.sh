40d3e043a5f16341fef5848149d9282682dec844
Felix Bechstein <felix.bechstein@otto.de>
2016-12-07 09:53:27 +0100

    ubuntu update renamed displays

--- bin/monitors.sh
+++ bin/monitors.sh
@@ -4,9 +4,9 @@ xrandr -q &> /dev/null
 
 if [ "${1}" = "off" ] ; then
   xrandr \
-    --output eDP1 --mode 1366x768 \
-    --output DP2-2 --off \
-    --output DP2-3 --off \
+    --output eDP-1 --mode 1366x768 \
+    --output DP-2-2 --off \
+    --output DP-2-3 --off \
     --output HDMI2 --off \
     --output VIRTUAL1 --off \
     --output DP1 --off \
@@ -16,21 +16,21 @@ if [ "${1}" = "off" ] ; then
     --output VGA1 --off
 elif [ "${1}" = 'tv' ] ; then
   xrandr \
-    --output eDP1 --mode 1366x768 --pos 0x0 \
-    --output VGA1 --mode 1360x768 --right-of eDP1
+    --output eDP-1 --mode 1366x768 --pos 0x0 \
+    --output VGA1 --mode 1360x768 --right-of eDP-1
 elif [ "${1}" = 'pair' ] ; then
   xrandr \
-    --output eDP1 --mode 1366x768 --pos 0x0 \
-    --output DP2-2 --mode 1680x1050 --pos 1680x0 \
-    --output DP2-3 --mode 1680x1050 --same-as DP2-2
-elif $(xrandr --prop | grep -q "DP2-2 connected"); then
+    --output eDP-1 --mode 1366x768 --pos 0x0 \
+    --output DP-2-2 --mode 1680x1050 --pos 1680x0 \
+    --output DP-2-3 --mode 1680x1050 --same-as DP-2-2
+elif $(xrandr --prop | grep -q "DP-2-2 connected"); then
   xrandr \
-    --output eDP1 --mode 1366x768 --pos 0x0 \
+    --output eDP-1 --mode 1366x768 --pos 0x0 \
     --output DP2-2 --mode 1680x1050 --pos 1680x0
   xrandr \
-    --output eDP1 --mode 1366x768 --pos 0x0 \
-    --output DP2-2 --mode 1680x1050 --pos 1680x0 \
-    --output DP2-3 --mode 1680x1050 --pos 3360x0 \
+    --output eDP-1 --mode 1366x768 --pos 0x0 \
+    --output DP-2-2 --mode 1680x1050 --pos 1680x0 \
+    --output DP-2-3 --mode 1680x1050 --pos 3360x0 \
     --output HDMI2 --off \
     --output VIRTUAL1 --off \
     --output DP1 --off \
@@ -40,9 +40,9 @@ elif $(xrandr --prop | grep -q "DP2-2 connected"); then
     --output VGA1 --off
 else #if it's not connected
   xrandr \
-    --output eDP1 --mode 1366x768 \
-    --output DP2-2 --off \
-    --output DP2-3 --off \
+    --output eDP-1 --mode 1366x768 \
+    --output DP-2-2 --off \
+    --output DP-2-3 --off \
     --output HDMI2 --off \
     --output VIRTUAL1 --off \
     --output DP1 --off \
